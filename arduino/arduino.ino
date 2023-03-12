9410b20f33ad83efb33bb4aa5505328a07eb9cb1
Rodroids Team <rodroids@academiadeinfo.ro>
2023-03-12 13:21:52 +0200

    sketch for PID

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,26 +5,23 @@
 
 #define MAX_SPEED 255
 
+#define SET_POINT 5.0 // desired value of sensor output
+#define KP 1     // proportional constant
+#define KI 0.0  // integrative constant
+#define KD 0    // derivative constant
+
 void setup() {
     // put your setup code here, to run once:
     pinMode(PWML, OUTPUT);
     pinMode(PWMR, OUTPUT);
     pinMode(MY_5V, OUTPUT);
     digitalWrite(MY_5V, HIGH);
+    // TODO: make speed more clever
+    set_speed(50);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    for(int i=-255; i<255; i++) {
-        set_speed(i);
-        delay(50);
-    }
-    delay(500);
-    for(int i=255; i>-255; i--) {
-        set_speed(i);
-        delay(50);
-    }
-//  analogWrite(PWMR, 0);
+    set_direction(PID_output());
 }
 
 void set_speed(int my_speed) {
@@ -43,4 +40,24 @@ void set_speed(int my_speed) {
         digitalWrite(PWML, LOW);
     }
     return;
+}
+
+void set_direction(int position) {
+    // TODO: control servo
+}
+
+double get_error() {
+    double sensor_value;
+    // TODO: read sensor
+    return (sensor_value-SET_POINT);
+}
+
+double PID_output() {
+    static double last_error = 0;
+    static double integral = 0;
+    double error = get_error();
+    double derivative;
+    integral += error;
+    derivative = last_error - error;
+    return (error*KP + integral*KI + derivative*KD);
 }
