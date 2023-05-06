e65346715336c998f600fb82d75b5b386c2b24fb
Rodroids Team <rodroids@academiadeinfo.ro>
2023-05-06 07:58:55 +0300

    local_phase

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -26,16 +26,17 @@ int blueColor = 0;
 
 #define MY_5V 8
 
-#define MAX_SPEED 130
+#define MAX_SPEED 100
+#define MIN_SPEED 60
 #define MIN_SERVO 30
 #define MAX_SERVO 150
 #define SERVO_PIN 9
 
-double set_point 30.0 // desired value of sensor output
-double kp 4.0
-double ki 0.15  // integrative constant
-double kd 7.5    // derivative constant
-double damp 0.5
+double set_point = 30.0; // desired value of sensor output
+double kp = 3.0;
+double ki = 0.05;  // integrative constant
+double kd = 0.0;  // derivative constant
+double damp = 0.5;
 #define LOGS true
 
 Servo myservo;
@@ -87,11 +88,13 @@ void loop() {
         String incomingString = Serial.readStringUntil('\n');
         Serial.print("I received: ");
         Serial.println(incomingString);
+        Serial.write("incomingString");
         int a, b, c, d;
         sscanf(incomingString.c_str(), "%d %d %d %d", &a, &b, &c, &d);
-        sp=a/1000.0, kp=b/1000.0, ki=c/1000.0, kd=d/1000.0;
+        set_point=a/1000.0, kp=b/1000.0, ki=c/1000.0, kd=d/1000.0;
     }
     int output = PID_output();
+    set_speed(max(MIN_SPEED, MAX_SPEED-abs(output)));
     set_direction(output);
 }
 
@@ -124,12 +127,12 @@ double get_error_tof() {
     sensor.read();
     static double output = 0;
     double sensor_value = sensor.ranging_data.range_mm/10.0;
-    if(LOGS){
-        Serial.print(" Sensor: ");
-        Serial.print(sensor_value);
-        Serial.print(" Sensor status: ");
-        Serial.print(sensor.ranging_data.range_status);
-    }
+    // if(LOGS){
+    //     Serial.print(" Sensor: ");
+    //     Serial.print(sensor_value);
+    //     Serial.print(" Sensor status: ");
+    //     Serial.print(sensor.ranging_data.range_status);
+    // }
     if (sensor.ranging_data.range_status != 0) sensor_value = set_point+20;
     else if (sensor_value > set_point+20) sensor_value = set_point+20;
     else if (sensor_value < set_point-20) sensor_value = set_point-20;
@@ -139,19 +142,19 @@ double get_error_tof() {
 
 int PID_output() {
     double error = get_error_tof();
-    if(LOGS) {
-        Serial.print(" Error: ");
-        Serial.print(error);
-    }
+    // if(LOGS) {
+    //     Serial.print(" Error: ");
+    //     Serial.print(error);
+    // }
     static double integral = 0;
     integral = integral * damp + error;
     static double last_error = 0;
     double derivative = error - last_error;
     last_error = error;
     int output = int(error*kp + integral*ki + derivative*kd);
-    if(LOGS) {
-        Serial.print(" Output: ");
-        Serial.println(output);
-    }
+    // if(LOGS) {
+    //     Serial.print(" Output: ");
+    //     Serial.println(output);
+    // }
     return output;
 }
